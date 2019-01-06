dccbe58c9bd8a03eb4058d30dd2d2d66f3c294f9
David Pecot <dp@northernocean.net>
2019-01-06 11:54:00 -0500

    Add Customer Controller for list of customers

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+		// GET: Customer
+
+		[Route("customers/{index?}")]
+		public ActionResult Index()
+        {
+
+			CustomerViewModel customers = new CustomerViewModel()
+			{
+				Customers = new List<Customer>()
+					{
+						new Customer {Name = "Customer1"},
+						new Customer {Name = "Customer2"}
+
+					}
+			};
+
+			return View(customers);
+        }
+    }
+}
