501825affa1f1272f6059a29560e0a74cb05d2a9
David Pecot <dp@northernocean.net>
2019-01-13 17:39:47 -0500

    Customer Edit Form

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+using System.Data.Entity;
+
+namespace Vidly.Controllers
+{
+	public class CustomersController : Controller
+	{
+
+		private readonly ApplicationDbContext _context;
+
+		public CustomersController()
+		{
+			_context = new ApplicationDbContext();
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			_context.Dispose();
+		}
+
+		// GET: Customer
+		public ActionResult Index()
+		{
+
+			var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+
+			return View(customers.ToList());
+		}
+
+		//[Route("customers/details/{id}")]
+		//public ActionResult Details(int id)
+		//{
+		//	var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
+
+		//	if (customer == null)
+		//		return HttpNotFound();
+
+		//	return View(customer);
+		//}
+
+		//public ActionResult New()
+		//{
+		//	var membershipTypes = _context.MembershipTypes.ToList();
+		//	var viewModel = new CustomerFormViewModel
+		//	{
+		//		MembershipTypes = membershipTypes
+		//	};
+
+		//	return View("CustomerForm", viewModel);
+		//}
+
+		[HttpPost]
+		public ActionResult Save(Customer customer)
+		{
+			if (customer.Id == 0)
+			{
+				_context.Customers.Add(customer);
+			}
+			else
+			{
+				var customerInDB =
+						_context.Customers.SingleOrDefault(c => c.Id == customer.Id);
+				customerInDB.Name = customer.Name;
+				customerInDB.Birthdate = customer.Birthdate;
+				customerInDB.MembershipTypeId = customer.MembershipTypeId;
+				customerInDB.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+			}
+			_context.SaveChanges();
+			////return RedirectToAction("Index", "Customers");
+			return Content("saved");
+		}
+
+		public ActionResult Edit(int id)
+		{
+			var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+			if(customer == null)
+			{
+				return HttpNotFound();
+			}
+
+			var viewModel = new CustomerFormViewModel
+			{
+				Customer = customer,
+				MembershipTypes = _context.MembershipTypes.ToList()
+			};
+
+			return View("CustomerForm", viewModel);
+		}
+
+    }
+}
--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -8,11 +8,17 @@ namespace Vidly.Models
 {
 	public class MembershipType
 	{
+
 		public byte Id { get; set; }
-		public short SignUpFee { get; set; }
+
 		public byte DurationInMonths { get; set; }
+
 		public byte DiscountRate { get; set; }
+
 		[StringLength(100)]
 		public string Name { get; set; }
+
+		public short SignUpFee { get; set; }
+
 	}
 }
